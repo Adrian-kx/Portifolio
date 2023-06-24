1b7d34af62ccdfb39a4dfdc3cf497610d552d6b7
Adrian-kx <adriankxavier@gmail.com>
2023-06-24 00:48:14 -0300

    modified:   assets/css/index.css 	modified:   assets/css/scroolbar.css 	modified:   assets/css/variables.css 	new file:   assets/img/eu.jpeg 	new file:   assets/img/euMaga.jpeg 	modified:   assets/js/index.js 	modified:   index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,46 @@
-(function (){
-    function desativarDragDrop () {
-      document.addEventListener('dragstart', (evento) => evento.preventDefault(), false)
-      document.addEventListener('drop', (evento) => evento.preventDefault(), false)
-    }
-    desativarDragDrop()
-  }())
+(function () {
+  function desativarDragDrop() {
+    document.addEventListener('dragstart', (evento) => evento.preventDefault(), false)
+    document.addEventListener('drop', (evento) => evento.preventDefault(), false)
+  }
+  desativarDragDrop()
+}())
 
 //o que vai ser escrito no Home
 
-var texto = "Desenvolvedor Web Front End."; 
+var texto = "Desenvolvedor Web Front End.";
 var tempoEscrita = 100;
 
 var elementoTexto = document.getElementById("animationTextHome");
 var indiceCaractere = 0;
 
 function escreverTexto() {
-    elementoTexto.textContent += texto.charAt(indiceCaractere);
-    indiceCaractere++;
+  elementoTexto.textContent += texto.charAt(indiceCaractere);
+  indiceCaractere++;
 
-    if (indiceCaractere < texto.length) {
-        setTimeout(escreverTexto, tempoEscrita);
-    }
+  if (indiceCaractere < texto.length) {
+    setTimeout(escreverTexto, tempoEscrita);
+  }
 }
 
 escreverTexto();
 
-
-setTimeout(function() {
+// tempo de subida e descida da imagem no home
+setTimeout(function () {
   var imagem = document.getElementById("imgAnimationHome");
   imagem.style.animationPlayState = "running";
-}, 2000); 
+}, 2000);
+
+
+// header fixo
+
+window.addEventListener('scroll', function () {
+  var header = document.getElementById('header');
+  var scrollPosition = window.scrollY;
+
+  if (scrollPosition > 0) {
+    header.classList.add('header-scrolled');
+  } else {
+    header.classList.remove('header-scrolled');
+  }
+});
